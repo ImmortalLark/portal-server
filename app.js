0214ffe67b51b993f4c76dfb35245043669adc17
ImmortalLark <frorice@163.com>
2018-09-04 18:16:04 +0800

    增加body解析，端口创建校验和请求数据转发

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
  * @Author: Feng fan
  * @Date: 2018-09-03 14:37:21
  * @Last Modified by: Feng fan
- * @Last Modified time: 2018-09-03 19:12:49
+ * @Last Modified time: 2018-09-04 16:30:17
  */
 const Koa = require('koa');
+const koaBody = require('koa-body');
 const Router = require('koa-router');
 const ServerManager = require('./lib/server-manager');
 
@@ -16,19 +17,24 @@ const router = new Router();
 const serverManager = new ServerManager();
 
 // api request 
-router.get('/api/connect', async (ctx) => {
-    const { subdomain, server } = serverManager.createServer();
+router.get('/portal/connect', async (ctx) => {
+    let { subdomain, port } = ctx.query;
+    const result = serverManager.createServer({ subdomain, port });
+    const { server } = result;
     const address = await server.address;
+    subdomain = result.subdomain;
+    port = address.port;
     ctx.body = { domain: `${subdomain}.${supdomain}`, port: address.port };
 });
 
+app.use(koaBody());
 app.use(router.routes());
 app.use(router.allowedMethods());
 // 转发请求至对应的客户端
 app.use(async (ctx, next) => {
     const subdomain = ctx.host.split(`.${supdomain}`)[0];
     const server = serverManager.getServer(subdomain);
-    server && server.transmit();
+    server && server.transmit(ctx);
     await next();
 });
 
