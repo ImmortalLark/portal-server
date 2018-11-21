a0a913642a9fda58d023f375c3d29bdd3fff7cc9
ImmortalLark <frorice@163.com>
2018-11-21 21:00:15 +0800

    重构server端，改为websocket连接

--- app.js
+++ app.js
@@ -2,58 +2,59 @@
  * @Author: Feng fan
  * @Date: 2018-09-03 14:37:21
  * @Last Modified by: Feng fan
- * @Last Modified time: 2018-11-19 16:37:19
+ * @Last Modified time: 2018-11-21 20:54:21
  */
 const Koa = require('koa');
-const koaBody = require('koa-body');
 const Router = require('koa-router');
-const ServerManager = require('./lib/server-manager');
+const ConnectionManager = require('./lib/connection-manager');
+const logger = require('./lib/utils/logger');
 
 const PORT = +process.env.ENV_PORT; // set ENV_PORT=xxx
 const supdomain = process.argv[2] || 'portal.qa.igame.163.com';
 
 const app = new Koa();
 const router = new Router();
-const serverManager = new ServerManager();
+const httpServer = require('http').Server(app.callback());
+const io = require('./lib/portal-socket')(httpServer);
+let connectionManager = new ConnectionManager(io);
 
 // api request 
-router.get('/portal/connect', async (ctx) => {
-    let { subdomain, port } = ctx.query;
-    const result = serverManager.createServer({ subdomain, port });
-    const { server } = result;
-    const address = await server.agent.address;
-    ctx.body = { 
-        subdomain: `${result.subdomain}`,
-        port: address.port
-    };
+router.get('/portal/connect', async (ctx, next) => {
+  let { subdomain } = ctx.query;
+  subdomain = connectionManager.createConnection({ subdomain, ctx });
+  // subdomain有可能更新过，所以用返回的subdomain
+  ctx.body = { 
+    subdomain
+  };
 });
 
-app.use(koaBody());
 app.use(router.routes());
 app.use(router.allowedMethods());
 // 转发请求至对应的客户端
 app.use((ctx) => {
-    ctx.respond = false;
-    const subdomain = ctx.host.split(`.${supdomain}`)[0];
-    const server = serverManager.getServer(subdomain);
-    if (!server) {
-        ctx.body = {
-            code: 500,
-            msg: 'error: no server'
-        }
-        return console.info('error: no server');
+  ctx.respond = false;
+  const subdomain = ctx.host.split(`.${supdomain}`)[0];
+  const connection = connectionManager.getConnection(subdomain);
+  if (!connection) {
+    ctx.body = {
+      code: 500,
+      msg: 'error: no connection'
     }
-    if (server.agent.destroyed) {
-        serverManager.removeServer(subdomain);
-        ctx.body = {
-            code: 500,
-            msg: 'error: server destroyed'
-        }
-        return console.info('error: server destroyed');
+    return logger.error('no server');
+  }
+  if (!connection.established) {
+    ctx.body = {
+      code: 500,
+      msg: 'error: connecting'
     }
-    server.transmit(ctx);
+    return logger.error('no connection');
+  }
+  connection.transmit(ctx.req);
 });
 
 app.listen(PORT || 3000, () => {
-    console.log(`开始监听${PORT || 3000}端口`);
+  console.log(`开始监听${PORT || 3000}端口`);
+})
+httpServer.listen(2000, () => {
+  console.log(`socket.io 开始监听2000端口`);
 });
